b37d5fb432d1003e8901db6018bbba26b7176d64
praveen1466 <praveensknl7@gmail.com>
2020-11-24 14:05:03 -0800

    Added: Shows that how many times win

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,11 +1,22 @@
 #! /bin/bash
 echo "Welcome"
 
-flip=$(( RANDOM%2 ))
+read -p "enter number for number of flippin coin" num
 
-if(( $flip == 1 ))
-then
-	echo "Tail"
-else
-	echo "head"
-fi
+hCount=0
+tCount=1
+for(( i=0; i<$num; i++ ))
+do
+
+	flip=$(( RANDOM%2 ))
+
+	if(( $flip == 1 ))
+	then
+		hCount=$(( hCount+1 ))
+	else
+		tCount=$(( tCount+1 ))
+	fi
+done
+
+echo "Head is won $hCount times"
+echo "Tail is won $tCount times"
