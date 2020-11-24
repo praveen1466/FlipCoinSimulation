2b5aaef57f05e20e7cbd42e060bf3ca3cef58d27
praveen1466 <praveensknl7@gmail.com>
2020-11-24 14:19:58 -0800

    Added: 21 times or if win they who won and by how much

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -4,19 +4,39 @@ echo "Welcome"
 read -p "enter number for number of flippin coin" num
 
 hCount=0
-tCount=1
-for(( i=0; i<$num; i++ ))
-do
+tCount=0
+tmp=1
 
+while(( $tmp != 0 ))
+do
 	flip=$(( RANDOM%2 ))
 
 	if(( $flip == 1 ))
 	then
-		hCount=$(( hCount+1 ))
+		tCount=(( tCount++))
 	else
-		tCount=$(( tCount+1 ))
+		hCount=(( hCount++))
+	fi
+
+	if(( $tCount == 21 | $hCount == 21 ))
+	then
+		tmp=0
 	fi
 done
 
-echo "Head is won $hCount times"
-echo "Tail is won $tCount times"
+diff=$tCount-$hCount
+
+if(( $tCount==21 ))
+then
+	echo " Tail is $tCount times win"
+elif(( $hCount==21 ))
+	echo "Head is $hCount times win"
+
+elif((  $tCount > $hCount ))
+	diff=$tCount-$hCount
+	echo "Tail is won by $diff score"
+else
+	diff=$hCount-$tCount
+	echo "Head is won by $diff score"
+fi
+
